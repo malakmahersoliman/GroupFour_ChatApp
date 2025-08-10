08160270040ce790762a40deea9750a587246bf7
Kareem Abd El-Shakour <kareemabshakour@gmail.com>
2025-08-10 20:01:52 +0300

    Finished Private Chat Feature

--- src/PrivateChat.cpp
+++ src/PrivateChat.cpp
@@ -1,5 +1,6 @@
 
 #include "../include/PrivateChat.h"
+#include<iostream>
 
 using namespace std;
 // ========================
@@ -9,13 +10,33 @@ using namespace std;
 
 PrivateChat::PrivateChat(string u1, string u2) {
     // TODO: Implement constructor
+	user1 = u1;
+	user2 = u2;
+
+	participants.push_back(u1);
+	participants.push_back(u2);
+
 }
 
 void PrivateChat::displayChat() const{
     // TODO: Implement private chat display
+
+	if (messages.empty())
+	{
+		cout << "No Messages \n";
+		return;
+	}
+	else
+	{
+		for (int i = 0; i<messages.size(); i++)
+		{
+			messages[i].display();
+		}
+	}
 }
 
 void PrivateChat::showTypingIndicator(const std::string& username) const {
     // TODO: Implement typing indicator
+	cout << username << "Typing...";
 }
 
